c790ec2999e9fb79d3e15a37643ed967507e3b48
johnnyjoestar <ryan.goudjil@gmail.com>
2016-04-11 01:46:34 -0400

    Flying Couch
    Sprint 2 Update

---
+++ TruckDrive/app/src/main/java/com/example/ryangoud/truckdrive/AvailableActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryangoud.truckdrive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvailableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available);
+    }
+}
